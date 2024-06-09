ef5c11772e3335e239a45c6ee2441ea4cdfe2daa
gustavoqooo <luisangel.salvat@gmail.com>
2024-06-08 21:22:17 -0500

    typescript de ejercicio pipe

--- src/app/form-pipes/form-pipes.component.ts
+++ src/app/form-pipes/form-pipes.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-pipes.component.html',
   styleUrl: './form-pipes.component.css'
 })
 export class FormPipesComponent {
+  alumnos = [
+    {nombre: "Luis Angel"},
+    {nombre: "Luis Alberto"},
+    {nombre: "Luis Raúl"},
+    {nombre: "Luis Gabriel"}
+  ]
+  fechaActual = new Date()
+  cursos = [
+    {nombre: "Desarrollo de Aplicaciones Moviles 2", precio: 3200},
+    {nombre: "Desarrollo de Servicios Web 2", precio: 2500},
+    {nombre: "Base de datos 2", precio: 2850},
+    {nombre: "Inglés Técnico", precio: 1850},
+  ]
+  numeroDecimal = 128.55858782
 
 }
